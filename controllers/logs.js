887a78eb8acd4acc1d5ef277a59dce3eb997cb7d
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2026-03-11 19:23:29 +0100

    Moved logs logic into a controller

---
+++ controllers/logs.js
@@ -0,0 +1,34 @@
+// Endpoint to receive access logs from ESP32
+const addLog = (req, res) => {
+  const { event, message } = req.body;
+
+  if (!event && !message)
+    throw new BadRequestError("Log is missing event or message information.");
+
+  console.log(`\n[ALERT] New Log Received!`);
+  console.log(`Event: ${event}`);
+  console.log(`Message: ${message}`);
+
+  let esp32Command = "";
+
+  // Extract just the UID string from the message
+  const scannedUid = message.split(": ")[1];
+
+  if (event === "RFID Scan") {
+    if (allowedUIDs.includes(scannedUid)) {
+      console.log(`✅ Access Granted for ${scannedUid}!`);
+      esp32Command = "OPEN_DRAWER"; // Send the OPEN_DRAWER command
+    } else {
+      console.log(`❌ Access Denied for ${scannedUid}!`);
+    }
+  }
+
+  // Respond back to ESP32 to confirm log
+  res.status(200).json({
+    status: "success",
+    message: "Log saved to backend",
+    logBody: { event, message },
+  });
+};
+
+module.exports = { addLog };
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const uidsRouter = require("./routes/uids");
+const BadRequestError = require("./errors/bad-request");
+
 require("dotenv").config();
 
 const app = express();
@@ -9,69 +12,8 @@ app.get("/api/v1/hello", (req, res) => {
   res.json({ message: "Hello from be" });
 });
 
-// Endpoint to receive access logs from ESP32
-app.post("/api/v1/log", (req, res) => {
-  const { event, message } = req.body;
-  if (!event && !message) throw new Error("Error occured in request body");
-
-  console.log(`\n[ALERT] New Log Received!`);
-  console.log(`Event: ${event}`);
-  console.log(`Message: ${message}`);
-
-  let esp32Command = "";
-
-  // Extract just the UID string from the message
-  const scannedUid = message.split(": ")[1];
-
-  if (event === "RFID Scan") {
-    if (allowedUIDs.includes(scannedUid)) {
-      console.log(`✅ Access Granted for ${scannedUid}!`);
-      esp32Command = "OPEN_DRAWER"; // Send the OPEN_DRAWER command
-    } else {
-      console.log(`❌ Access Denied for ${scannedUid}!`);
-    }
-  }
-
-  // Respond back to ESP32 to confirm log
-  res.status(200).json({
-    status: "success",
-    message: "Log saved to backend",
-    logBody: { event, message },
-  });
-});
-
-// local "DB" for testing
-let allowedUIDs = [];
-
-// Endpoint to manage allowed UIDs
-// TODO : Implement Database so that there is not a local variable
-// UID is sent with a allowed: boolean in request body
-app.post("/api/v1/uid", (req, res) => {
-  const { uid, allowed } = req.body;
-  if (!uid || typeof allowed !== "boolean") {
-    return res
-      .status(400)
-      .json({ error: "Valid UID and allowed boolean are required." });
-  }
-
-  if (allowed) {
-    if (!allowedUIDs.includes(uid)) allowedUIDs.push(uid);
-  } else {
-    allowedUIDs = allowedUIDs.filter((id) => id !== uid);
-  }
-
-  const uidMsg = allowed
-    ? `Access granted to card with UID:${uid}`
-    : `Access removed from card with UID:${uid}`;
-
-  res.status(200).json({
-    status: "success",
-    message: uidMsg,
-    uidBody: { uid, allowed },
-  });
-});
-
-// TODO GET ALL SCANNED CARDS TO THIS POINT
+app.use("/api/v1/uids", uidsRouter);
+app.use("/api/v1/logs", logsRouter);
 
 // Start the server
 const start = async () => {
