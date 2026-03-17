95be4d1f8629d8666c5edb889e2d6c643a9d08ec
cuprliky <bruhova.nikol@gmail.com>
2026-03-17 12:40:23 +0100

    routa get_one_card ok

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -3,7 +3,7 @@ const Card = require("../models/card");
 
 //vsechny api routy
 
-exports.get_all_cards = async (req, res) => {
+const get_all_cards = async (req, res) => {
   try {
     const cards = await Card.find({}); 
     
@@ -13,6 +13,25 @@ exports.get_all_cards = async (req, res) => {
   }
 };
 
+const get_one_card = async (req, res) => {
+  try {
+    const { id } = req.params; 
 
+    const card = await Card.findById(id); 
+
+    if (!card) {
+      return res.status(404).json({ message: "Karta nebyla nalezena" });
+    }
+
+    res.status(200).json(card);
+  } catch (error) {
+    res.status(500).json({ message: "Chyba při načítání karty", error: error.message });
+  }
+};
+
+module.exports = {
+  get_all_cards,
+  get_one_card
+}
 
 
--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -1,9 +1,9 @@
 //routy co komunikuji s IoT
 const express = require("express");
 const apiController = require("../controllers/cardController");
-console.log("DEBUG - Typ get_all_cards:", typeof apiController.get_all_cards);
 const router = express.Router();
 
 router.get("/", apiController.get_all_cards);
+router.get("/:id", apiController.get_one_card);
 
 module.exports = router;
