b7893554a9e18055da5cc67b77e85f849ff5e582
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2026-04-27 12:44:57 +0200

    Fixed export error

--- tools/encrypt.js
+++ tools/encrypt.js
@@ -1,7 +1,7 @@
 const crypto = require("crypto");
 
 // --- Encryption Utility (Moved to the Cloud!) ---
-export function encrypt(text) {
+const encrypt = (text) => {
   // Uses the ENCRYPTION_KEY from your Render environment variables
   const keyStr = process.env.ENCRYPTION_KEY || "fallback_key_do_not_use_in_prod";
   const key = crypto.createHash('sha256').update(String(keyStr)).digest('base64').substring(0, 32);
@@ -15,3 +15,5 @@ export function encrypt(text) {
     Buffer.concat([cipher.update(text), cipher.final()]).toString("hex")
   );
 }
+
+module.exports = encrypt;
