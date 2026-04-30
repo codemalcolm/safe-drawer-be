8584218b33c4aa901835c2238719813e887aa308
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2026-04-30 11:25:22 +0200

    Added fields to Device schema

--- models/Device.js
+++ models/Device.js
@@ -6,6 +6,11 @@ const DeviceSchema = new mongoose.Schema({
   raspberryPiId: { type: String, required: true },
   isLocked: { type: Boolean, required: true, default: true },
   timestamp: { type: Date, default: Date.now },
+  isOnline: { type: Boolean, default: false },
+  hasIncident: { type: Boolean, default: false },
+  lastUsed: { type: String, default: Date.now },
+  createdAt: { type: String, default: Date.now },
+  updatedAt: { type: String, default: Date.now },
 });
 
 module.exports = mongoose.model("Device", DeviceSchema);
