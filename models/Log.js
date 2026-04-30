36610bd3580d561d9aa83a35a986517aa811d27f
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2026-04-30 16:12:14 +0200

    Remade Log schema

--- models/Log.js
+++ models/Log.js
@@ -1,1 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+const LogSchema = new Schema({
+  event: { type: String, required: true },
+  type: { type: String, required: true, default: "info" },
+  drawerId: {
+    type: Schema.Types.ObjectId,
+    ref: "Device",
+    required: true, // card MUST be assigned to a drawer
+  },
+  message: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Log", LogSchema);
