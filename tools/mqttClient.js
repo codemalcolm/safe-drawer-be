0bf05c91f429f38b767377765d303a4c9d0cb2fe
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2026-05-03 14:14:14 +0200

    Added mqtt subscribe to drawer/logs and a listener

--- tools/mqttClient.js
+++ tools/mqttClient.js
@@ -1,6 +1,8 @@
 const mqtt = require("mqtt");
+const Device = require("../models/Device");
+const Log = require("../models/Log");
 
-const client = mqtt.connect({
+const mqttClient = mqtt.connect({
   host: process.env.MQTT_CLUSTER_URL || "host-url",
   port: 8883,
   protocol: "mqtts",
@@ -8,5 +10,47 @@ const client = mqtt.connect({
   password: process.env.MQTT_PASSWORD || "",
 });
 
-client.on("connect", () => console.log("✅ Connected to MQTT Broker"));
-module.exports = client;
+mqttClient.on("connect", () => {
+  console.log("Ⓜ️  Backend successfully connected to MQTT Broker!");
+  // Subscribes:
+  mqttClient.subscribe("drawer/logs");
+});
+
+// TOPIC : drawer/logs
+mqttClient.on("message", async (topic, payload) => {
+  if (topic === "drawer/logs") {
+    try {
+      const data = JSON.parse(payload.toString());
+      const { raspberryPiId, event, message, type } = data;
+
+      // Find DB Object using the Hardware ID
+      const linkedDevice = await Device.findOne({
+        raspberryPiId: raspberryPiId,
+      });
+
+      if (!linkedDevice) {
+        console.warn(
+          `[SECURITY] Ignored log from unknown hardware: ${raspberryPiId}`,
+        );
+        return;
+      }
+
+      console.log(linkedDevice);
+
+      // Save it to the database
+      const newLog = await Log.create({
+        drawerId: linkedDevice._id,
+        event: event,
+        type: type || "info",
+        message: message,
+      });
+
+
+      console.log(
+        `[MQTT] Nový log uložen do DB pro šuplík: ${newLog.drawerId}`,
+      );
+    } catch (error) {
+      console.error("Error processing MQTT log:", error.message);
+    }
+  }
+});
